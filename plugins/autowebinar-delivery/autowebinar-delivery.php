2a38e833ce42353a476764f45363aa000774c968
vsepro100-info <vsepro100.info@gmail.com>
2026-01-18 06:17:32 -0800

    chore: remove diagnostic probe from autowebinar

--- plugins/autowebinar-delivery/autowebinar-delivery.php
+++ plugins/autowebinar-delivery/autowebinar-delivery.php
@@ -10,15 +10,6 @@ defined('ABSPATH') || exit;
 add_action('core_user_registered', 'autowebinar_delivery_on_user_registered');
 add_action('core_user_login', 'autowebinar_delivery_on_user_login');
 add_action('core_ref_context_resolved', 'autowebinar_delivery_on_ref_context_resolved');
-add_action('core_user_login', function ($user_id, $user = null) {
-    error_log('[autowebinar][probe] core_user_login fired user_id=' . intval($user_id));
-});
-// @diagnostic REMOVE AFTER TEST
-
-add_action('core_user_registered', function ($user_id) {
-    error_log('[autowebinar][probe] core_user_registered fired user_id=' . intval($user_id));
-});
-// @diagnostic REMOVE AFTER TEST
 
 function autowebinar_delivery_on_user_registered() {
     error_log('[autowebinar] core_user_registered fired');
