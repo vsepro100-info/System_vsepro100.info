1ce04e4f6ae352fe31b84d044eb60399ed550c71
vsepro100-info <vsepro100.info@gmail.com>
2026-01-20 04:09:57 -0800

    Add Telegram notifier for lead ingest

---
+++ plugins/service-telegram/service-telegram.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Plugin Name: Service Telegram Notifier
+ * Description: Sends Telegram notification on core_lead_ingest.
+ * Version: 0.1.0
+ */
+
+defined('ABSPATH') || exit;
+
+add_action('core_lead_ingest', 'service_telegram_on_lead_ingest', 10, 1);
+
+function service_telegram_on_lead_ingest(array $lead_meta) {
+    if (!defined('TELEGRAM_BOT_TOKEN') || !defined('TELEGRAM_CHAT_ID')) {
+        return;
+    }
+
+    $webinar_id = $lead_meta['webinar_id'] ?? '';
+    $ref = $lead_meta['ref'] ?? '';
+    $ip = $lead_meta['ip'] ?? '';
+
+    $message = "New autowebinar lead:\n"
+        . "webinar_id: {$webinar_id}\n"
+        . "ref: {$ref}\n"
+        . "ip: {$ip}";
+
+    $url = 'https://api.telegram.org/bot' . TELEGRAM_BOT_TOKEN . '/sendMessage';
+
+    wp_remote_post($url, [
+        'body' => [
+            'chat_id' => TELEGRAM_CHAT_ID,
+            'text' => $message,
+        ],
+    ]);
+}
