fdc081f9150d04c7590c933dbb8d3cd16337fb06
vsepro100-info <vsepro100.info@gmail.com>
2026-01-27 12:37:04 -0800

    Fix webinar state REST permissions

--- plugins/ui-webinar-room/ui-webinar-room.php
+++ plugins/ui-webinar-room/ui-webinar-room.php
@@ -22,23 +22,32 @@ function ui_webinar_room_register_state_routes() {
             array(
                 'methods' => WP_REST_Server::READABLE,
                 'callback' => 'ui_webinar_room_handle_state_read',
-                'permission_callback' => 'ui_webinar_room_can_read_state',
+                'permission_callback' => 'ui_webinar_room_can_access_state',
             ),
             array(
                 'methods' => WP_REST_Server::CREATABLE,
                 'callback' => 'ui_webinar_room_handle_state_update',
-                'permission_callback' => 'ui_webinar_room_can_update_state',
+                'permission_callback' => 'ui_webinar_room_can_access_state',
             ),
         )
     );
 }
 
-function ui_webinar_room_can_read_state() {
-    return is_user_logged_in();
-}
+/**
+ * @param WP_REST_Request $request
+ * @return bool
+ */
+function ui_webinar_room_can_access_state($request) {
+    if (!is_user_logged_in()) {
+        return false;
+    }
 
-function ui_webinar_room_can_update_state() {
-    return is_user_logged_in() && (current_user_can('speaker') || current_user_can('manage_options'));
+    $method = strtoupper($request->get_method());
+    if ($method === WP_REST_Server::READABLE || $method === 'GET' || $method === 'HEAD') {
+        return true;
+    }
+
+    return current_user_can('speaker') || current_user_can('manage_options');
 }
 
 function ui_webinar_room_get_current_webinar_id() {
