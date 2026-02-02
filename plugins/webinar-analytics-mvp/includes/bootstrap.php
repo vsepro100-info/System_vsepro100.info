46b1a8486a8dedddd92f6338a99b6cc391c88f85
vsepro100-info <vsepro100.info@gmail.com>
2026-02-02 02:50:11 -0800

    feat(webinars): add mvp analytics hooks

---
+++ plugins/webinar-analytics-mvp/includes/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+defined('ABSPATH') || exit;
+
+require_once __DIR__ . '/module.php';
+
+add_action('webinar_started', 'webinar_analytics_mvp_handle_webinar_started', 10, 1);
+add_action('user_attended', 'webinar_analytics_mvp_handle_user_attended', 10, 1);
+add_action('webinar_finished', 'webinar_analytics_mvp_handle_webinar_finished', 10, 1);
+add_action('cta_shown', 'webinar_analytics_mvp_handle_cta_shown', 10, 1);
+add_action('cta_clicked', 'webinar_analytics_mvp_handle_cta_clicked', 10, 1);
